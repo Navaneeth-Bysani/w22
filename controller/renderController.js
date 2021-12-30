8439cf59b0cc0927ce56a30a56a9e79a7c63dd18
Navaneeth Bysani <61292292+Navaneeth-Bysani@users.noreply.github.com>
2021-12-30 20:07:54 +0530

    made changes in signing up and login page

---
+++ controller/renderController.js
@@ -0,0 +1,39 @@
+exports.renderAbout = (req,res) => {
+    //render About
+}
+
+exports.renderCompetitions = (req,res) => {
+    //render Competitions
+}
+
+exports.renderWorkshops = (req,res) => {
+    //render Workshops
+}
+
+exports.renderGuestLectures = (req,res) => {
+    //render Guest Lectures
+}
+
+exports.renderExhibits = (req,res) => {
+    //render Exhibits
+}
+
+exports.renderInitiatives = (req,res) => {
+    //render Initiatives
+}
+
+exports.renderHighlights = (req,res) => {
+    //render Highlights
+}
+
+exports.renderTeam = (req,res) => {
+    //render Team
+}
+
+exports.renderSponsors = (req,res) => {
+    //render Sponsors
+}
+
+exports.renderContact = (req,res) => {
+    //render Contact
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const userController = require('../controller/userController');
+const renderController = require('../controller/renderController');
 const router = express.Router();
 
+//frontend routes
 router.get('/profile', userController.getProfile);
+router.get('/about', renderController.renderAbout);
+router.get('/competitions', renderController.renderCompetitions);
+router.get('/workshops', renderController.renderWorkshops);
+router.get('/guestlectures', renderController.renderGuestLectures);
+router.get('/exhibits', renderController.renderExhibits);
+router.get('/initiatives', renderController.renderInitiatives);
+router.get('/highlights', renderController.renderHighlights);
+router.get('/team', renderController.renderTeam);
+router.get('/sponsors', renderController.renderSponsors);
+router.get('/contact', renderController.renderContact);
+
+
+//individual competition routes
+
+//individual workshop routes
+
+//posting a query in contact
 
 module.exports = router;
