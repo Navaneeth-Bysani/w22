cf769652ce9089e40ed5852622a8405b9995a444
kartikeya-123 <56500864+kartikeya-123@users.noreply.github.com>
2022-01-10 01:18:35 +0530

    updated renderController

--- controller/renderController.js
+++ controller/renderController.js
@@ -1,42 +1,54 @@
-exports.renderAbout = (req,res) => {
-    //render About
-}
+exports.renderAbout = (req, res) => {
+  //render About
+  res.render("about");
+};
 
-exports.renderCompetitions = (req,res) => {
-    //render Competitions
-}
+exports.renderCompetitions = (req, res) => {
+  //render Competitions
+  res.render("competitions");
+};
 
-exports.renderWorkshops = (req,res) => {
-    //render Workshops
-}
+exports.renderWorkshops = (req, res) => {
+  //render Workshops
+  console.log("req");
+  res.render("workshop");
+};
 
-exports.renderGuestLectures = (req,res) => {
-    //render Guest Lectures
-}
+exports.renderGuestLectures = (req, res) => {
+  //render Guest Lectures
+  res.render("guestlectures");
+};
 
-exports.renderExhibits = (req,res) => {
-    //render Exhibits
-}
+exports.renderExhibits = (req, res) => {
+  //render Exhibits
+  res.render("initiatives");
+};
 
-exports.renderInitiatives = (req,res) => {
-    //render Initiatives
-}
+exports.renderInitiatives = (req, res) => {
+  //render Initiatives
+  res.render("initiatives");
+};
 
-exports.renderHighlights = (req,res) => {
-    //render Highlights
-    res.render('highlights');
-}
+exports.renderHighlights = (req, res) => {
+  //render Highlights
+  res.render("highlights");
+};
 
-exports.renderTeam = (req,res) => {
-    //render Team
-    res.render('team');
-}
+exports.renderTeam = (req, res) => {
+  //render Team
+  res.render("team");
+};
 
-exports.renderSponsors = (req,res) => {
-    //render Sponsors
-}
+exports.renderSponsors = (req, res) => {
+  //render Sponsors
+  res.render("sponsors");
+};
 
-exports.renderContact = (req,res) => {
-    //render Contact
-    res.render('contact');
-}
+exports.renderContact = (req, res) => {
+  //render Contact
+  res.render("contact");
+};
+
+exports.renderUser = (req, res) => {
+  res.render("profile.ejs");
+};
