97e0ab835e52ced7569f72baecd9914fb8fda96b
Navaneeth Bysani <61292292+Navaneeth-Bysani@users.noreply.github.com>
2022-03-03 16:56:33 +0530

    added few files and frontend changes

---
+++ utils/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config({path:'./../.env'});
+
+module.exports = {
+    MONGODB_URI : process.env.MONGODB_URI,
+    PORT : process.env.PORT,
+    NODE_ENV : process.env.NODE_ENV,
+    HOST : process.env.HOST,
+    EMAIL_PASSWORD : process.env.EMAIL_PASSWORD,
+    EMAIL : process.env.EMAIL,
+    SENDGRID_API_KEY : process.env.SENDGRID_API_KEY
+}
